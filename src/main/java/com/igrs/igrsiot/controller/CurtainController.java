99cdb574503a4a6dca35e7988d43bc5f3ff501f7
roger-tian <50897955@qq.com>
2018-05-22 18:04:02 +0800

    ""

--- src/main/java/com/igrs/igrsiot/controller/CurtainController.java
+++ src/main/java/com/igrs/igrsiot/controller/CurtainController.java
@@ -1,21 +1,61 @@
 package com.igrs.igrsiot.controller;
 
+import com.igrs.igrsiot.model.IgrsDeviceStatus;
+import com.igrs.igrsiot.model.IgrsOperate;
+import com.igrs.igrsiot.service.IIgrsDeviceStatusService;
+import com.igrs.igrsiot.service.IIgrsOperateService;
 import com.igrs.igrsiot.service.SocketService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/control")
 public class CurtainController {
+    @Autowired
+    private IIgrsDeviceStatusService igrsDeviceStatusService;
+    @Autowired
+    private IIgrsOperateService igrsOperateService;
+
     @RequestMapping("/curtain")
     public String curtainOnOff(String onOff) throws SQLException {
-        logger.debug("onOff: {}", onOff);
+        String instruction;
+
         String cmd = "{ch_60:" + onOff + "}";   // 0,1,2--off,on,pause
         SocketService.cmdSend(cmd);
+
+        IgrsDeviceStatus igrsDeviceStatus = new IgrsDeviceStatus();
+        igrsDeviceStatus.setDeviceId("curtain");
+        igrsDeviceStatus.setAttribute("switch");
+        igrsDeviceStatus.setValue(onOff);
+        IgrsDeviceStatus status = igrsDeviceStatusService.selectByDeviceIdAndAttribute(igrsDeviceStatus);
+        if (status != null) {
+            igrsDeviceStatusService.updateByDeviceIdAndAttribute(igrsDeviceStatus);
+        }
+        else {
+            igrsDeviceStatusService.insert(igrsDeviceStatus);
+        }
+
+        IgrsOperate igrsOperate = new IgrsOperate();
+        igrsOperate.setDeviceId("curtain");
+        igrsOperate.setUser("admin");
+        if (onOff.equals("1")) {
+            instruction = "开关打开";
+        }
+        else {
+            instruction = "开关关闭";
+        }
+        igrsOperate.setInstruction(instruction);
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String time = df.format(new Date());
+        igrsOperate.setOperateTime(time);
+        igrsOperateService.insert(igrsOperate);
 
         return "SUCCESS";
     }

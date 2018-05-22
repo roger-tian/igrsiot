20d0d8068407d4982ad4b3c5fd64edc90393b998
roger-tian <50897955@qq.com>
2018-05-22 10:04:08 +0800

    ""

---
+++ src/main/resources/sql/igrsiot.sql
@@ -0,0 +1,89 @@
+﻿-- MySQL dump 10.13  Distrib 5.1.73, for redhat-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: igrsiot
+-- ------------------------------------------------------
+-- Server version	5.7
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `igrs_operate`
+--
+
+use igrsiot;
+
+DROP TABLE IF EXISTS `igrs_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `igrs_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user` varchar(22) DEFAULT NULL,
+  `password` varchar(22) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=920 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `igrs_device_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `igrs_device_status` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `device_id` varchar(22) DEFAULT NULL,
+  `attribute` varchar(22) DEFAULT NULL,
+  `value` varchar(22) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=920 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `igrs_operate`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `igrs_operate` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user` varchar(22) DEFAULT NULL,
+  `operate_time` datetime DEFAULT NULL,
+  `device_id` varchar(22) DEFAULT NULL,
+  `instruction` varchar(22) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=920 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `igrs_sensor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `igrs_sensor` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `type` varchar(22) DEFAULT NULL,
+  `value` FLOAT(6,2) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `hour` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=920 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `igrs_sensor_detail`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `igrs_sensor_detail` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `type` varchar(22) DEFAULT NULL,
+  `value` FLOAT(10,3) DEFAULT NULL,
+  `time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=920 DEFAULT CHARSET=utf8;
+
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;

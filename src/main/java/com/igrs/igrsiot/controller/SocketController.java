5f11df8dc15ae05fa80d5070ac3ccdb0eeeb1f11
roger-tian <50897955@qq.com>
2018-06-04 17:46:31 +0800

    ""

--- src/main/java/com/igrs/igrsiot/controller/SocketController.java
+++ src/main/java/com/igrs/igrsiot/controller/SocketController.java
@@ -245,7 +245,7 @@ public class SocketController {
                     igrsSensorDetailService.insert(igrsSensorDetail);
                 }
                 else if (cells[0].contains("hcho")) {
-                    formaldehyde = cells[1];
+                    formaldehyde = cells[1].substring(0, cells[1].length()-2);
                     igrsSensorDetail.setType("formaldehyde");
                     igrsSensorDetail.setValue(formaldehyde);
                     logger.debug("formaldehyde: {}", igrsSensorDetail.getValue());

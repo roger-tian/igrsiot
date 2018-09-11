b71159b0c6a89e2743e29fa2839f0534756fd7a3
chixin <jixin1025@126.com>
2018-09-11 09:23:28 +0800

    测试提交git

--- src/main/java/com/igrs/igrsiot/controller/AllController.java
+++ src/main/java/com/igrs/igrsiot/controller/AllController.java
@@ -24,6 +24,7 @@ import java.util.List;
 @RequestMapping("/control")
 public class AllController {
     @RequestMapping("/all")
+
     public JSONObject allSwitch(@RequestHeader(value="igrs-token", defaultValue = "") String token,
             String room, String onOff) throws ParseException {
         IgrsToken igrsToken = igrsTokenService.getByToken(token);
